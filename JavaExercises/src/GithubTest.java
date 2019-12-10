546e79809d697497a76d6c2f0fd9707e6b772ba0
Sujin Lee <sjnu21@naver.com>
2019-12-10 16:33:02 +0900

    Update

--- JavaExercises/src/GithubTest.java
+++ JavaExercises/src/GithubTest.java
@@ -4,7 +4,7 @@ public class GithubTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, world");
-
+		Test
 	}
 
 }
--- JavaExercises/src/PrintStarPattern.java
+++ JavaExercises/src/PrintStarPattern.java
@@ -1,27 +1,47 @@
-
+import java.util.Scanner;
 public class PrintStarPattern {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the number of rows.");
+		int rows=scan.nextInt();
+		System.out.println("Enter the number of colums.\n(¡Ø Some of the patterns may not apply the number of column to exactly obey the pattern rule.)");
+		int columns=scan.nextInt();
 		
-		//Pyramid 
 		
-		// Increase Star Number from 1 to 5 
-		for(int i=1;i<=5;i++) {
+		// Pattern #1 - Rectangle
+		
+		// #1-1. Solid Rectangle	
+		for(int i=1;i<=rows;i++) {
+			System.out.println("*****");
+		}
+		System.out.println();
+		
+		// #1-2. Hollow Rectangle
+		
+		
+		
+		// Pattern #2 - Pyramid
+		
+		// #2-1. Half Pyramid
+		for(int i=1;i<=rows;i++) {
 			for(int j=1;j<=i;j++) {
-				System.out.printf("%c",'¡Ú');
+				System.out.printf("%c",'*');
 			}
 			System.out.println();
 		}
+		System.out.println();
 		
-		// Decrease Star Number from 5 to 1
-		for(int i=1;i<=5;i++) {
-			for(int j=5;j>=i;j--) {
-				System.out.printf("%c",'¡Ú');
+		// #2-2. Inverted Half Pyramid
+		for(int i=1;i<=rows;i++) {
+			for(int j=rows;j>=i;j--) {
+				System.out.printf("%c",'*');
 			}
 			System.out.println();
 		}
+		System.out.println();
 		
 	}
 
