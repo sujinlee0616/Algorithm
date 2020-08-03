cefb0388d644722ea0f02004d3e55dcdd609356e
Sujin Lee <sjnu21@naver.com>
2020-08-04 05:39:18 +0900

    boj tower solution2

--- JavaExercises/sujin/boj/boj_q2493_tower3.java
+++ JavaExercises/sujin/boj/boj_q2493_tower3.java
@@ -6,26 +6,20 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.Stack;
 
-// idea) 
-// 1. 위치와 높이 한 번에 다루기 힘드니까... 클래스로 만들어버리자. 
-//  - 위치(index)와, 높이(height)를 한 방에 처리할 수 있도록 Top 클래스를 만들자. 
-// 2. 탐색하는 경우의 수를 나눠보자. 
-//  - 탑을 1번부터 조사하는데.. 스택에다가 탑을 1번부터 넣어보고.. 
-// 1) 스택이 비어있다 ==> 왼쪽놈부터 집어넣자 
-// 2) 스택에 있는 놈(왼쪽)보다 내가 더 height가 크다 ==>  
-// 3) 스택에 있는 놈(왼쪽)보다 내가 더 height가 작다 ==> 
+/* idea) 
+ 1. 위치와 높이 한 번에 다루기 힘드니까... 클래스로 만들어버리자. 
+  - 위치(index)와, 높이(height)를 한 방에 처리할 수 있도록 Top 클래스를 만들자. 
+ 2. 탐색하는 방식을 분석해보자.
+  - 나보다 (1) 왼쪽에 있고 (2) 높이가 더 높으면 ===> 그 index를 출력한다. 
+  - 나보다 (1) 왼쪽에 있고 (2) 높이가 더 높더라도 (3) 더 가까운 쪽에 있는 탑의 index를 출력한다. 
+    ==> 높은 탑이면 저장해놨다가 저장하면 되겠네 
+    ==> Q. 높은 탑인걸 어떻게 판단할건데? 
+        A. arr[i]랑 비교하면 되겠네
+        Q. 높이가 높더라도 더 가까운 쪽에 있는 탑의 index 출력하는건 어떻게 할건데? 
+        A. Stack써서 하면 되겠네.  
+*/  
 
-/*
- *1) current 탑을 저장 스택에 저장하는 이유? LIFO하면서 안에 있는값이 큰지 작은지 판단 해야 함 .
-  2) 작은 쪽 인덱스 (왼쪽)에 current보다 큰 값이 있으면
-  3) stack에서 꺼내
-  4) 작은 값이라면
-  5) stack에 current를 담
-  6) 같은 값이라면
-  7) 무시  
-*/
-
-
+// 이 풀이방식은 평소 내 사고방식이랑은 크게 맞지 않는듯...
 public class boj_q2493_tower3 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -42,24 +36,30 @@ public class boj_q2493_tower3 {
 		String[] s2=s.split(" ");
 		
 		for (int i=1; i<=n; i++) { // 1부터 n까지 
-			arr[i]=Integer.parseInt(s2[i]);
+			arr[i]=Integer.parseInt(s2[i-1]);
 			//System.out.println("arr["+i+"]="+arr[i]);
 		}
 		
-		// Tower 클래스로 Stack을 만들자 
+		// Top 클래스로 Stack을 만들자 
 		Stack<Top> stk = new Stack<Top>();
 		
-		for(int i=1;i<=n;i++) {
-			if(stk.size()==0) {
-				stk.push(new Top(i,arr[i]));
+		for(int i=1; i<=n; i++) {
+			while(true) {
+				if(stk.size()==0) {
+					System.out.print(0+" ");
+					stk.push(new Top(i, arr[i]));
+					break;
+				}
+				else if(stk.peek().height < arr[i]) { // 스택에 들어가있는 애(왼쪽)이 나보다 작으면 
+					stk.pop();
+					// pop은 여러번 할 수 있으니까 break 걸지 X 
+				}
+				else if(stk.peek().height > arr[i]) { // 스택에 들어가있는 애(왼쪽)이 나보다 크면 
+					System.out.print(stk.peek().index+" ");
+					stk.add(new Top(i, arr[i]));
+					break;
+				}
 			}
-			if(stk.peek().height < arr[i]) { // 스택에 들어가있는 애(왼쪽)이 나보다 작으면 
-				stk.pop(); 
-			}
-			else if(stk.peek().height > arr[i]) { // 스택에 들어가있는 애(왼쪽)이 나보다 크면 
-				
-			}
-				
 		}
 	}
 	
