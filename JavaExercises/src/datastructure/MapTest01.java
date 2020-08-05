40ccfee8b8e9892edc52bca0a0b4969e5809af68
shimjaehyuk <jaehyukawot@gmail.com>
2020-08-06 00:59:52 +0900

    Map 공부하기 - Collection 사용법

---
+++ JavaExercises/src/datastructure/MapTest01.java
@@ -0,0 +1,29 @@
+package datastructure;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapTest01 {
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("a", 123);
+		map.put("b", 323);
+		map.put("c", 123);
+		map.put("e", 153);
+		map.put("d", 1263);
+		map.put("hello", 72);
+		map.put("a", 124);
+		map.remove("e");
+		System.out.println(map.get("c"));
+		System.out.println(map.get("notUsed"));
+		System.out.println("--------------------");
+		print(map);
+	}
+	private static void print(Map<String, Integer> map) {
+		Set<Map.Entry<String, Integer>> entries = map.entrySet();
+		for(Map.Entry<String, Integer> entry : entries) {
+			System.out.println("{" + entry.getKey() + " : " + entry.getValue() + "}");
+		}
+	}
+}
