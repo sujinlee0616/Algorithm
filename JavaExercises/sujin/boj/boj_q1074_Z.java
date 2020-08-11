5e8648e53935320b71552127840cbca575f5ba6b
Sujin Lee <sjnu21@naver.com>
2020-08-11 23:56:34 +0900

    Solve boj q1072

---
+++ JavaExercises/sujin/boj/boj_q1074_Z.java
@@ -0,0 +1,25 @@
+package boj;
+
+import java.util.Scanner;
+
+public class boj_q1074_Z {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		int m = (int) Math.pow(2, n); // m = 2^n 
+		// System.out.println("m="+m);
+		// int[][] map = new int[m][m];
+		
+		visit();
+		
+	}
+	
+	static void visit() {
+		
+	}
+
+}
--- JavaExercises/sujin/boj/boj_q1926_painting.java
+++
@@ -1,9 +0,0 @@
-package boj;
-
-public class boj_q1926_painting {
-	
-	public static void main(String[] args) {
-		
-	}
-
-}
