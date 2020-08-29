77130948e9bf33a18cbf693de1111548b3258611
shimjaehyuk <jaehyukawot@gmail.com>
2020-08-29 17:38:05 +0900

    순열을 NextPermutation을 이용해 구한다 1. 오름차순 정렬 2. 꼭대기 찾기 3. 꼭대기와 교환할 것 찾기 4. 교환하기 5. 오름차순으로 바꾸기

---
+++ JavaExercises/src/datastructure/NextPermutation.java
@@ -0,0 +1,65 @@
+package datastructure;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NextPermutation {
+	
+	private int N;
+	private int[] arr;
+	
+	public static void main(String[] args) {
+		new NextPermutation().service();
+	}
+	private void service() {
+		Scanner scn = new Scanner(System.in);
+		
+		N = scn.nextInt();
+		arr = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = scn.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		do {
+			print(arr);
+		} while(nextPermutation());
+		
+		scn.close();
+	}
+	
+	private boolean nextPermutation() {
+		int i = N-1;
+		while(i > 0 && arr[i] <= arr[i-1]) {
+			i--;
+		}
+		if(i == 0) return false;
+		
+		int j = N-1;
+		while(arr[i-1] >= arr[j]) {
+			j--;
+		}
+		
+		swap(arr, i-1, j);
+		
+		int k = N-1;
+		while(i < k) {
+			swap(arr, i++, k--);
+		}
+		return true;
+	}
+	
+	private void print(int[] arr) {
+		System.out.println("\n----------");
+		for(int i = 0; i < N; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println("\n----------");
+	}
+	private void swap(int[] arr, int a, int b) {
+		int tmp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = tmp;
+	}
+}
